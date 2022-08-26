a6e015f23d0f5e41e8731877aca73de9b93758b9
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-26 15:08:51 -0500

    create courses data

---
+++ 1c/src/data/courses.js
@@ -0,0 +1,44 @@
+export const courses = [
+  {
+    id: 1,
+    name: 'Fullstack roadmap',
+    parts: [
+      {
+        name: 'Fundamentals of Nodejs',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Express',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'MongoDB',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Frontend bootcamp',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 5,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 17,
+        id: 3
+      }
+    ]
+  }
+]
