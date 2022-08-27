b3f3c0a86934af0066d6c88103de48ce62453c5c
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-26 19:29:59 -0500

    change text to children prop

--- 1c/src/components/Button.jsx
+++ 1c/src/components/Button.jsx
@@ -1,10 +1,10 @@
-export const Button = ({ onclick, text, bgcolor, textcolor, hover = '' }) => {
+export const Button = ({ onclick, children, bgcolor, textcolor, hover = '' }) => {
   return (
     <button
       className={`${bgcolor} px-4 py-2 ${textcolor} py-2 px-5 rounded hover:${hover}`}
       onClick={onclick}
     >
-      {text}
+      {children}
     </button>
   )
 }
