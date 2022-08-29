9213f7104ef88977a567a8109b3b78c2209de67e
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-28 19:21:33 -0500

    change props

--- 1c/src/components/Note.jsx
+++ 1c/src/components/Note.jsx
@@ -1,7 +1,8 @@
-export const Note = ({ content = 'Content missing' }) => {
+export const Note = ({ title = 'Title missing', body = 'Body missing' }) => {
   return (
-    <li className='bg-slate-200 p-4 rounded-lg'>
-      <p>{content}</p>
+    <li className='bg-slate-200 p-4 rounded-lg space-y-4'>
+      <h2 className='text-xl font-semibold'>{title}</h2>
+      <p>{body}</p>
     </li>
   )
 }
