41eaa120ae0dd2a4e2134f3062890b0986f4433c
Oscar Estrada <arturostrada2020@gmail.com>
2022-08-28 20:15:27 -0500

    create loading component

---
+++ 1c/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className='absolute inset-0 w-full h-screen flex justify-center items-center bg-white'>
+      <span className='text-2xl tracking-wider'>Loading ...</span>
+    </div>
+  )
+}
